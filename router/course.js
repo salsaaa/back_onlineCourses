dd39d7b82d62a8d583436508203a8409285de02a
salsaaa <salsafci97@gmail.com>
2020-07-17 00:52:40 +0200

    fix images errors

--- router/course.js
+++ router/course.js
@@ -32,7 +32,7 @@ router.get("/filters&Pagination", async (req, res, next) => {
       .populate("categoryId");
     filters.map((c) => {
       if (c.img) {
-        const file = path.resolve(__dirname, `../public/${c.img}`);
+        const file = path.resolve(__dirname,'public',`${c.img}`);
 
         var bitmap = fs.readFileSync(file);
         const img = new Buffer(bitmap).toString("base64");
@@ -52,7 +52,7 @@ router.get("/filters&Pagination", async (req, res, next) => {
       .populate("categoryId");
     filters.map((c) => {
       if (c.img) {
-        const file = path.resolve(__dirname, `../public/${c.img}`);
+        const file = path.resolve(__dirname, 'public',`${c.img}`);
         var bitmap = fs.readFileSync(file);
         const img = new Buffer(bitmap).toString("base64");
         c["img"] = img;
@@ -78,7 +78,7 @@ router.get("/:id", authenticationMiddleware, async (req, res, next) => {
   const course = await Course.findById(id).populate("categoryId");
   let img = "";
   if (course.img) {
-    const file = path.resolve(__dirname, `../public/${course.img}`);
+    const file = path.resolve(__dirname, 'public',`${c.img}`);
     var bitmap = fs.readFileSync(file);
     img = new Buffer(bitmap).toString("base64");
   }
