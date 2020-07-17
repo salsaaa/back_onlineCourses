5380703f5a261c20fe183492fbd9c7694f60d6bc
salsaaa <salsafci97@gmail.com>
2020-07-17 04:51:56 +0200

    change course images store

--- router/search.js
+++ router/search.js
@@ -11,19 +11,19 @@ router.get("/courses", authenticationMiddleware, async (req, res, next) => {
   const currentUsers = await Course.find().select("_id title categoryId img").populate({
     path: "categoryId",
   });
-//  const users= currentUsers.map((u) => {
+ const users= currentUsers.map((u) => {
     
-//       if (u.img) {
-//           const file = path.resolve(__dirname,'public',`${u.img}`);
+      if (u.img) {
+          const file = path.resolve(__dirname, `../public/${u.img}`);
           
-//           var bitmap = fs.readFileSync(file);
-//           const img = new Buffer(bitmap).toString("base64");
-//           u["img"] = img;
-//         }
-//         return u
-//     });
+          var bitmap = fs.readFileSync(file);
+          const img = new Buffer(bitmap).toString("base64");
+          u["img"] = img;
+        }
+        return u
+    });
     
-  res.json(currentUsers);
+  res.json(users);
 });
 
 module.exports = router;
